6365090556b753c2113770943b6c7bf4523a9f83
Alejandro vargas cardoso <yepes03vargas@gmail.com>
2025-02-18 10:26:13 -0500

    Ejercicio # 1 y #2

---
+++ script.js
@@ -0,0 +1,12 @@
+let nombre = "Alejandro"
+let edad = "19"
+let ciudad = "Garzón"
+console.log("Hola, mi nombre es "+ nombre +", tengo "+ edad +" años y vivo en "+ ciudad +".");
+
+/* crea una variable llamada precio y asignale un valor decimal. luego, crea otra variable llamada cantidad y asignale un valor entero. muestra el tipo de dato de cada variable en la consola.*/
+
+let precio = 19.99;
+let cantidad = 5;
+console.log(typeof precio);//Imprime en la consola
+document.write(typeof cantidad);//Imprime en la consola
+alert(typeof cantidad);// Imprime en la consola 
